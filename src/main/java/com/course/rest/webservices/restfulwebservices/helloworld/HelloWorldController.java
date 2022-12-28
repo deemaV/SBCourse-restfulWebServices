48ae14e49631952b6fa19e826145c4aa4e09f5cc
Deema AlMutairi <116001326+deemaV@users.noreply.github.com>
2022-12-28 10:33:19 +0300

    Version 1

---
+++ src/main/java/com/course/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,28 @@
+package com.course.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Controller
+@RestController
+public class HelloWorldController {
+	
+
+	@GetMapping(path= "/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+
+	// hello-world-bean
+	@GetMapping(path= "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World!");
+	}
+	// hello-world/path-variable/Deema
+		@GetMapping(path= "/hello-world/path-variable/{name}")
+		public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+			return new HelloWorldBean(String.format("Hello World, %s", name));
+		}
+}
---
+++ src/main/java/com/course/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,49 @@
+package com.course.rest.webservices.restfulwebservices.user;
+import java.util.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 3;
+	static {
+		users.add(new User(1,"Deema",new Date()));
+		users.add(new User(2,"Leena",new Date()));
+		users.add(new User(3,"Reema",new Date()));
+
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User save(User user) {
+		if(user.getId() == null) {
+			user.setId(++usersCount);
+		}
+		users.add(user);
+		
+		return user;
+	}
+	
+	public User findOne(int id) {
+		for(User user : users) {
+			if(user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		while(iterator.hasNext()) {
+			User user = iterator.next();
+			if(user.getId() == id) {
+				iterator.remove();
+				return user;
+			}
+		}
+		return null;
+	}
+}
